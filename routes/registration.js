2c1749cea1ecc1690228980ab2c18e900d7c85b1
ksgunasekera <ksgunasekera@gmail.com>
2017-11-01 06:20:54 +0530

    Added regiserUser.ejs

--- routes/registration.js
+++ routes/registration.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router=express.Router();
 
 router.get('/',(request,respond)=>{
-	respond.render('registration');
+	respond.render('registerUser');
 });
 
 router.post('/register',(request,respond)=>{
