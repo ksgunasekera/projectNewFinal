00bab041bc539112c966c9d11e01a56047c07a33
ksgunasekera <ksgunasekera@gmail.com>
2017-11-07 06:01:03 +0530

    Registration fields validation

--- routes/registration.js
+++ routes/registration.js
@@ -2,14 +2,21 @@ const express=require('express');
 const router=express.Router();
 
 var nicError=false;
-var usernameError=true;
+var usernameError=false;
+var emailEmptyError=false;
+var emailError=false;
+var passwordEmptyError=false;
+var passwordLenError=false;
+var confirmPasswordEmpty=false;
 
 router.get('/',(request,respond)=>{
-	respond.render('registerUser',{error:true,nicError:nicError,usernameError:true});
+	respond.render('registerUser',{error:true,nicError:nicError,usernameError:usernameError,emailEmptyError:emailEmptyError,emailError:emailError,passwordEmptyError:passwordEmptyError,passwordLenError:passwordLenError,confirmPasswordEmpty:confirmPasswordEmpty});
 	request.session.errors=null;
 });
 
 router.post('/register',(request,respond)=>{
+	request.check('nic').equals("");
+
 	console.log(request.body.username);
 });
 
