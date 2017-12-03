7698437b89b3d69db4a5cb1f37186306a7129e0f
ksgunasekera <ksgunasekera@gmail.com>
2017-12-03 21:12:43 +0530

    Added admin panel

--- routes/registration.js
+++ routes/registration.js
@@ -23,30 +23,38 @@ router.get('/submit',(request,respond)=>{
 
 router.post('/submit',(request,respond)=>{
 	console.log("POST");
+	
 	request.check('nic',"1").isLength({min:10,max:12});//
 	request.check('username',"2").isLength({min:6});
-	request.check('email',"3").isEmail();
-	request.check('paswword',"4").isLength({min:8}).equals(request.body.confirmPassword);
+	request.check('email',"3").isEmail();//isLength({min:7});
+	request.check('paswword',"4")
 	
+
 	var errors=request.validationErrors();
 	console.log(errors.length);
 	if(errors){
 		request.session.errors=errors;
 		for (i in errors){
+			console.log(errors[i].msg);
 			if("1"==errors[i].msg){
 				nicError=true;
+				console.log(errors[i].msg);
 			}else if("2"==errors[i].msg){
 				usernameError=true;
+				console.log(errors[i].msg);
 			}else if("3"==errors[i].msg){
-				emailError==true;
+
+				emailError=true;
+				console.log(errors[i].msg);
 			}else if("4"==errors[i].msg){
 				passwordError=true;
 			}
 		}
-	
+		
 	}
 	request.session.errors=errors;
-	respond.redirect('/registration');
+		respond.redirect('/registration');
+	
 });
 
 module.exports =router;
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path=require('path');
 const login=require('./routes/login');
 const registration=require('./routes/registration');
 const forgetPassword=require('./routes/forgetPassword');
+const admin=require('./routes/admin');
+
 const bodyParser=require('body-parser');
 const expressSession =require('express-session');
 const expressValidator=require('express-validator');
@@ -17,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/js',express.static(__dirname+'/assets/js'));
 app.use('/css',express.static(__dirname+'/assets/css'));
+app.use('/fonts',express.static(__dirname+'/assets/fonts'));
 app.use('/images',express.static(__dirname+'/assets/images'));
 
 app.use(expressValidator());
@@ -42,6 +45,7 @@ app.set('view engine','ejs');
 app.use('/',login);
 app.use('/registration',registration);
 app.use('/forgetPassword',forgetPassword);
+app.use('/admin',admin);
 
 app.listen(port,()=>{
 	console.log('Server running at port :'+port);
