ced01baf4530ae8ce2fb7367b75dc4a8d3d29266
sasdul <32093645+sasdul@users.noreply.github.com>
2017-12-07 22:09:16 +0530

    update prescribe with add new function for create pdf report

--- doctor/routes/getpatient.js
+++ doctor/routes/getpatient.js
@@ -3,17 +3,15 @@ var router=express.Router();
 var connection = require('../modules/dbConnection');
 
 
-router.get('/:user_id',function(req,res,next){
+router.get('/',function(req,res,next){
 
-	var user_id = req.params.user_id;
-	console.log('Select nic is : '+user_id);
-	var sql1="select * from patient where user_id = '"+user_id+"'";
-	var sql2="delete from patient where user_id = '"+user_id+"'";
+	var sql1="select * from patient limit 1"; 
 	connection.query(sql1,function(err,rows){
 		    if (err){
 		    	throw err;
 		    }else{
 		      res.render('getpatient',{data:rows});
+		      var sql2="delete from patient where user_id = '"+rows[0].user_id+"'";
               connection.query(sql2,function(err,rows){
                       if (err){
 		    	          throw err;
